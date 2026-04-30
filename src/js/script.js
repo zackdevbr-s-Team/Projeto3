b7bf43e00692e441a338f06ba3976e8df1c1f0a0
Isac <contatoisacn@gmail.com>
2026-04-30 11:51:17 -0300

    feat: Função efeito hero

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
+window.onload = function(){
+    const intro = document.getElementById("inicio");
+
+    setTimeout(()=>{
+        intro.classList.add("efeito-out");
+        setTimeout(()=>{
+            intro.style.display="none";
+            content.classList.remove("hidden");
+            content.classList.add("efeito-in");
+        },1000)
+    },3000)
+}
+
 function trocar(cor){
     document.body.style.background=cor;
 }
